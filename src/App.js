88bc62267a166a2f70f556fd89f9b007e4a84d65
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-16 13:21:05 +0300

    card added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-
-import './App.css';
-
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Angular from "./images/angular.jpg";
+import Course from './Course';
+import Bootstrap from "./images/bootstrap5.png";
+import Csharp from "./images/ccsharp.png";
+import Kompleweb from "./images/kompleweb.jpg";
 
 function App() {
   return (
     <div>
 
         <AppBar>
-
-          <Toolbar>
-            Kart Projesi
-          </Toolbar>
-
+          <Toolbar sx= {{fontSize:30}}>Kart Projesi</Toolbar>
         </AppBar>
 
+        <Container>
+            <Grid container spacing={2} sx={{marginTop : 10}}>
+                  <Grid item xs={12} md={6} lg={3}>
+                    <Course 
+                     image={Angular}
+                     title= "Angular"
+                     description= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, vel alias sint doloremque ducimus nisi voluptatem tempore repellendus numquam odit ad repellat aperiam maxime dignissimos non ipsa nesciunt, voluptates officiis!"
+                    />
+                  </Grid>
 
+                  <Grid item xs={12} md={6} lg={3}>
+                  <Course 
+                     image={Bootstrap}
+                     title= "Bootstrap"
+                     description= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, vel alias sint doloremque ducimus nisi voluptatem tempore repellendus numquam odit ad repellat aperiam maxime dignissimos non ipsa nesciunt, voluptates officiis!"
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={6} lg={3}>
+                  <Course 
+                     image={Csharp}
+                     title= "Csharp"
+                     description= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, vel alias sint doloremque ducimus nisi voluptatem tempore repellendus numquam odit ad repellat aperiam maxime dignissimos non ipsa nesciunt, voluptates officiis!"
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={6} lg={3}>
+                  <Course 
+                     image={Kompleweb}
+                     title= "Kompleweb"
+                     description= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, vel alias sint doloremque ducimus nisi voluptatem tempore repellendus numquam odit ad repellat aperiam maxime dignissimos non ipsa nesciunt, voluptates officiis!"
+                    />
+                  </Grid>
+            </Grid>
+        </Container>
+        
     </div>
   );
 }
--- src/Course.js
+++ src/Course.js
@@ -1,11 +1,30 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 
-
-function Course() {
+function Course({image , title , description}) {
     return (
-        <div>
+      <Card >
+      <CardMedia
+        component="img"
+        height= "140"
+        image={image}
+        title={title}
+        alt = "Kurslarım"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+              {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+            {description}
+        </Typography>
+      </CardContent>
+     
+    </Card>
 
-        </div>
       );
 }
 
